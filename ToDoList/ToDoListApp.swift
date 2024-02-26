f09cb0888d2a82054a83f831cdd10ef22a3b65a0
Farajah <farajah3433@gmail.com>
2024-02-26 22:06:59 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Farajah Ebenezar on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
