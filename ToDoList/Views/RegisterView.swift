54230177456efd03c3e4e109497ee8daf8838e24
Farajah <farajah3433@gmail.com>
2024-03-03 00:28:16 +0300

    -Added the form on the RegisterView, -Made a custom button in the TLButton SwiftUI file to help in reusing in various pages if needed

--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             //Header
             HeaderView(title: "Register", subtitle: "Start Organizing todos", angle: -15, background: .red)
             
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .autocapitalization(.none)
+                SecureField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(
+                    title: "Register",
+                    background: .green
+                ){
+                    //Attempt Log in
+                }
+                .padding()
+            }
+            .offset(y: -50)
             Spacer()
         }
     }
