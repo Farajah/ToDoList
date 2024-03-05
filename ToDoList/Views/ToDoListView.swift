46990dc5cd9275c1f85c5060613162710d0547f7
Farajah <farajah3433@gmail.com>
2024-03-05 23:33:19 +0300

    Created the navigation tabs (Home & Profile) after logged in

--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -1,20 +1,36 @@
 //
 //  ToDoListItemsView.swift
 //  ToDoList
-//
-//  Created by Farajah Ebenezar on 29/02/2024.
+////  Created by Farajah Ebenezar on 29/02/2024.
 //
 
 import SwiftUI
 
 struct ToDoListView: View {
-    var body: some View {
-        Text("Welcome to your account!")
+    @StateObject var viewModel = ToDoListViewViewModel()
+    
+    private let userId: String
+    init(userId: String) {
+        self.userId = userId
     }
-}
-
-struct ToDoListItemsView_Previews: PreviewProvider {
-    static var previews: some View {
-        ToDoListView()
+        var body: some View {
+            NavigationView {
+                VStack {
+                }
+                .navigationTitle("To Do List")
+                .toolbar {
+                    Button {
+                        //Action
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
     }
-}
+    
+    struct ToDoListItemsView_Previews: PreviewProvider {
+        static var previews: some View {
+            ToDoListView(userId: "")
+        }
+    }
