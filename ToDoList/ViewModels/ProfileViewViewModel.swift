50473b55de236cda5f09e2fba60af4d469b2b480
Farajah <farajah3433@gmail.com>
2024-03-13 03:17:49 +0300

    - Done check circles/box - Updates live in the database - Even after closing and opening the app the box ticked remains ticked - Also changed the color to blue

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -5,12 +5,27 @@
 //  Created by Farajah Ebenezar on 29/02/2024.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
         
     }
 }
