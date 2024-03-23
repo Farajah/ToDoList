8814fc0bcc43945356962d0e7e153f64a4b8359f
Farajah <farajah3433@gmail.com>
2024-03-23 19:21:05 +0300

    - Fetching user details to the profile page done - Log out button functional and redirects you to the login page

--- ToDoList/ViewModels/ToDOListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDOListItemViewViewModel.swift
@@ -5,10 +5,29 @@
 //  Created by Farajah Ebenezar on 29/02/2024.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 ///ViewModel for single to do list item view (each row in items list
 ///Primary tab
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+        
+    }
 }
